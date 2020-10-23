dd1249e19333a62f66ced5b7395be6e08f81a9ed
Janeiro <psiqui99@gmail.com>
2020-10-23 09:34:48 -0400

    Web Server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+var hbs = require('hbs');
+require('./hbs/helpers');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//express hbs engine
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', function(req, res) {
+    res.render('home', {
+        nombre: 'JanEiro'
+    });
+});
+
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
